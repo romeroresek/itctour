4ba7bab13411a9d1fa220e66d2e2537197479855
Juan Javier ROMERO RESEK <106100100+romeroresek@users.noreply.github.com>
2025-10-30 23:38:20 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
       <div class="small">© <span id="y"></span> Itapúa Tenis Club · Todos los derechos reservados</div>
       <div class="d-flex align-items-center gap-3">
         <a href="https://www.instagram.com/itapuatenisclubok/#" class="small"><i class="bi bi-instagram me-1"></i>Instagram</a>
-        <a href="https://wa.me/54376576332" class="btn btn-sm btn-red">Whatsapp</a>
+        <a href="https://wa.me/543764576332" class="btn btn-sm btn-red">Whatsapp</a>
       </div>
     </div>
   </footer>

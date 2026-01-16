3194a516f8928b76cdff2dd30a840853ace15403
Juan Javier ROMERO RESEK <106100100+romeroresek@users.noreply.github.com>
2026-01-16 10:20:24 -0300

    Update header.php

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
             <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="torneos.php">Torneos1</a>
+            <a class="nav-link" href="torneos.php">Torneos</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Ranking</a>
